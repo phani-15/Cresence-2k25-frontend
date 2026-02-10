c9b0e0406a0ba562f95d2023bf9491b5b4fffea9
phani-15 <phanipolavarapu15@gmail.com>
2026-02-11 00:46:48 +0530

    half of the workshops page and events page is completed

--- src/Pages/HomeScene.jsx
+++ src/Pages/HomeScene.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import EntryScene from "../Components/EntryScene";
+import Workshops from "./Workshops";
 
 export default function HomeScene() {
   return (
     <>
       <div className="night-bg">
-
         <Navbar />
         <div>
-          <div className="min-h-screen min-w-screen">
+          <div className="min-h-screen">
             <EntryScene />
           </div>
-          
         </div>
       </div>
     </>
